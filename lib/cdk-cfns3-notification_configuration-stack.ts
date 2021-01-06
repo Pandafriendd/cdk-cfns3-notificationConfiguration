b6ae3b34803634c64de83c0d5b769a5e8c8e5b15
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-01-06 23:25:02 +0000

    source code commited

--- lib/cdk-cfns3-notification_configuration-stack.ts
+++ lib/cdk-cfns3-notification_configuration-stack.ts
@@ -1,9 +1,35 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CdkCfns3NotificationConfigurationStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        // The code that defines your stack goes here
+
+        const handler = new lambda.Function(this, 'MyFunc', {
+            code: lambda.Code.fromInline('boom'),
+            handler: 'index.handler',
+            runtime: lambda.Runtime.NODEJS_12_X,
+        });
+
+        /*
+        const s3Bucket = new s3.CfnBucket(this, "S3Bucket", {
+          websiteConfiguration: {
+            indexDocument: "index.html",
+            errorDocument: "index.html",
+          },
+        });
+        */
+
+        const s3Bucket2 = new s3.CfnBucket(this, "S3Bucket2", {
+            notificationConfiguration: { lambdaConfigurations: [{ event: 's3:ObjectCreated:*', function: handler.functionArn }] },
+        });
+
+
+        //new s3.Bucket(this, 'MyFirstBucket');
+
+
+    }
 }
