d16c6fea4d43684e16cedb2e1d4cb5b7cefad29d
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-01-07 23:22:11 +0000

    add lamda permission

--- lib/cdk-cfns3-notification_configuration-stack.ts
+++ lib/cdk-cfns3-notification_configuration-stack.ts
@@ -22,13 +22,23 @@ export class CdkCfns3NotificationConfigurationStack extends cdk.Stack {
           },
         });
         */
-
+        
+        
         const s3Bucket2 = new s3.CfnBucket(this, "S3Bucket2", {
             notificationConfiguration: { lambdaConfigurations: [{ event: 's3:ObjectCreated:*', function: handler.functionArn }] },
         });
+        
+        const lambdaPermission = new lambda.CfnPermission(this, "LambdaPermission", {
+            action: 'lambda:InvokeFunction',
+            principal: 's3.amazonaws.com',
+            sourceArn: s3Bucket2.attrArn,
+            functionName: handler.functionArn,
+        });
+        
 
-
-        //new s3.Bucket(this, 'MyFirstBucket');
+        //const bucket1 = new s3.Bucket(this, 'MyFirstBucket');
+        
+        
 
 
     }
